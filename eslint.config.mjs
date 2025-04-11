98dce0d2cc2910bf1687d0e2014669d7e4dde6b4
Sukhrob Abdullaev <sukhrobabdullaev@kookmin.ac.kr>
2025-04-12 01:06:20 +0900

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  extends: ['next'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        'argsIgnorePattern': '^_',
-        'varsIgnorePattern': '^_', 
-        'caughtErrorsIgnorePattern': '^_'
-      }
-    ],
-    '@typescript-eslint/no-require-imports': 'off',
-    '@typescript-eslint/no-unused-expressions': 'warn',
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off'
-  },
-  ignorePatterns: [
-    'lib/generated/prisma/**/*',
-    'node_modules/**/*'
-  ],
-  overrides: [
-    {
-      files: ['lib/generated/prisma/**/*'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/no-require-imports': 'off'
-      }
-    }
-  ]
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  }),
 ];
-
 export default eslintConfig;
