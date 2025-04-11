a42482f1a753f41cdf13b67540327da2e878eb3a
Sukhrob Abdullaev <sukhrobabdullaev@kookmin.ac.kr>
2025-04-12 00:55:33 +0900

    eslint prisma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,14 +9,36 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
   extends: ['next'],
   rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_', 
+        'caughtErrorsIgnorePattern': '^_'
+      }
+    ],
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-unused-expressions': 'warn',
     'react/no-unescaped-entities': 'off',
-    '@next/next/no-page-custom-font': 'off',
+    '@next/next/no-page-custom-font': 'off'
   },
+  ignorePatterns: [
+    'lib/generated/prisma/**/*',
+    'node_modules/**/*'
+  ],
+  overrides: [
+    {
+      files: ['lib/generated/prisma/**/*'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-require-imports': 'off'
+      }
+    }
+  ]
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  
 ];
 
 export default eslintConfig;
